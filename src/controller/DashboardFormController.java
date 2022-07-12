a7ff13cf15b40a0bbe0cabb33286e029328ac457
Milan-sandeepa <glmsandeepa@gmail.com>
2022-07-12 12:09:59 +0530

    Load all Student method Implemented

--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -2,11 +2,19 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import model.Student;
+import util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DashboardFormController {
     public AnchorPane root;
@@ -17,7 +25,7 @@ public class DashboardFormController {
     public JFXTextField txtStudentName;
     public JFXTextField txtStudentAddress;
     public JFXTextField txtStudentContact;
-    public TableView tblStudent;
+    public TableView<Student> tblStudent;
     public TableColumn colId;
     public TableColumn colName;
     public TableColumn colEmail;
@@ -26,6 +34,43 @@ public class DashboardFormController {
     public TableColumn colNic;
     public JFXTextField txtStudentEmail;
     public JFXTextField txtStudentNic;
+
+    public void initialize(){
+
+        colId.setCellValueFactory(new PropertyValueFactory("id"));
+        colName.setCellValueFactory(new PropertyValueFactory("name"));
+        colEmail.setCellValueFactory(new PropertyValueFactory("email"));
+        colContact.setCellValueFactory(new PropertyValueFactory("contact"));
+        colAddress.setCellValueFactory(new PropertyValueFactory("address"));
+        colNic.setCellValueFactory(new PropertyValueFactory("nic"));
+
+        try {
+            loadAllStudents();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadAllStudents() throws SQLException, ClassNotFoundException {
+        ResultSet result = CrudUtil.execute("SELECT * FROM Student");
+        ObservableList<Student> obList = FXCollections.observableArrayList();
+
+        while (result.next()){
+            obList.add(
+                    new Student(
+                            result.getString("student_id"),
+                            result.getString("student_name"),
+                            result.getString("email"),
+                            result.getString("contact"),
+                            result.getString("address"),
+                            result.getString("nic")
+                    )
+            );
+        }
+        tblStudent.setItems(obList);
+    }
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
 
