bf5e961fcf982039a8f530ef13ebe8e1a9418820
Milan-sandeepa <glmsandeepa@gmail.com>
2022-07-12 11:54:42 +0530

    Dashboad UI Updated

--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -1,7 +1,45 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class DashboardFormController {
     public AnchorPane root;
+    public JFXButton btnSave;
+    public JFXButton btnUpdate;
+    public JFXButton btnDelete;
+    public JFXTextField txtStudentId;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtStudentAddress;
+    public JFXTextField txtStudentContact;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+    public JFXTextField txtStudentEmail;
+    public JFXTextField txtStudentNic;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void textFields_Key_Released(KeyEvent keyEvent) {
+
+    }
 }
